d4bb2cf4fee7c61d13ca238e392e7879f48118a1
Vaibhav Jha <vaibhavjha22@gmail.com>
2024-08-21 14:29:39 +0530

    added header

---
+++ src/components/header.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button"
+import Link from "next/link"
+
+export default function Header() {
+  return (
+    <div className="sticky top-0 z-20 border-b bg-white">
+      <nav className="container flex h-16 items-center justify-between gap-4 text-sm">
+        <Link href={"/"} className="text-lg font-bold">
+          ZED DASHY
+        </Link>
+        <Button variant="outline">Add Widgets +</Button>
+      </nav>
+    </div>
+  )
+}
