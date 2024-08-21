de33293b8b61177faf3fbb49d3c53ea5884d82b0
Vaibhav Jha <vaibhavjha22@gmail.com>
2024-08-21 14:31:26 +0530

    added footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
+import Footer from "@/components/footer"
 import Header from "@/components/header"
 
 const inter = Inter({ subsets: ["latin"] })
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className="min-h-dvh bg-slate-50 font-sans antialiased">
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   )
