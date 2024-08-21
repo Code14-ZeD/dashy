61cdd2efa87dfd7f0ad6cdb80a12eaa44b7c10b4
Vaibhav Jha <vaibhavjha22@gmail.com>
2024-08-21 14:27:42 +0530

    created cards with json file

---
+++ src/app/categories.ts
@@ -0,0 +1,30 @@
+export const categories = [
+  {
+    name: "espm",
+    widgets: [
+      { id: 0, title: "gogo" },
+      { id: 1, title: "fofo" },
+    ],
+  },
+  {
+    name: "pasm",
+    widgets: [
+      { id: 0, title: "gogo" },
+      { id: 1, title: "fofo" },
+    ],
+  },
+  {
+    name: "casm",
+    widgets: [
+      { id: 0, title: "gogo" },
+      { id: 1, title: "fofo" },
+    ],
+  },
+  {
+    name: "lasm",
+    widgets: [
+      { id: 0, title: "gogo" },
+      { id: 1, title: "fofo" },
+    ],
+  },
+]
